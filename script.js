2401604afb70dc8a37555bb527146a05dc4932a2
squallerq <john199502031@gmail.com>
2024-09-19 22:55:48 +0200

    feat: add basic layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const realProgress = document.querySelector("#real-progress");
+const hiddenProgress = document.querySelector("#hidden-progress");
+const playButton = document.querySelector("#play-btn");
+
+console.log(realProgress);
+console.log(hiddenProgress);
+console.log(playButton);
+
