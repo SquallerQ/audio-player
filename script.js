d3727ebe0e4557be66aff51eaa74848a8d797190
squallerq <john199502031@gmail.com>
2024-09-20 23:10:16 +0200

    feat: add progressbar and play-pause function

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-const realProgress = document.querySelector("#real-progress");
-const hiddenProgress = document.querySelector("#hidden-progress");
+const audio = document.querySelector("#audio");
+const progressBar = document.querySelector("#progress-bar");
+const progressContainer = document.querySelector(".progress-container");
 const playButton = document.querySelector("#play-btn");
 
-console.log(realProgress);
-console.log(hiddenProgress);
-console.log(playButton);
+
+
+const songsArray = ['function', 'razor_bones', 'windows-on']
+let songIndex = 1;
+let isPlay = false;
+
+function renderSong (song) {
+  audio.src = `audio/${song}.mp3`
+}
+renderSong(songsArray[songIndex]);
+
+playButton.addEventListener('click', function () {
+  if (isPlay === false) {
+    console.log('d');
+    playAudio()
+    isPlay = true;
+  } else {
+    console.log('h');
+    pauseAudio();
+    isPlay = false;
+  }
+})
+
+function playAudio () {
+  audio.play();
+  playButton.classList.remove('pause')
+  playButton.classList.add('play')
+}
+function pauseAudio () {
+  audio.pause();
+  playButton.classList.add('pause')
+  playButton.classList.remove('play')
+}
+
+function changeProgressBar (e) {
+  const duration = e.srcElement.duration;
+  const currentTime = e.srcElement.currentTime;
+
+   if (!isNaN(duration)) {
+  const progressPercent = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressPercent}%`;
+   }
+  console.log("update");
+}
+
+audio.addEventListener("timeupdate", changeProgressBar);
+
 
