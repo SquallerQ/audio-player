a2d662425668fb46873fcb7b9517bc5401e6a981
squallerq <john199502031@gmail.com>
2024-09-21 11:50:08 +0200

    feat: add time indicators

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const audio = document.querySelector("#audio");
 const progressBar = document.querySelector("#progress-bar");
 const progressContainer = document.querySelector(".progress-container");
 const playButton = document.querySelector("#play-btn");
+const trackTime = document.querySelector('.max-time')
+const currentTimeElement = document.querySelector('.current-time');
 
 
 
 const songsArray = ['function', 'razor_bones', 'windows-on']
-let songIndex = 1;
+let songIndex = 0;
 let isPlay = false;
 
 function renderSong (song) {
@@ -14,13 +16,23 @@ function renderSong (song) {
 }
 renderSong(songsArray[songIndex]);
 
+
+function correctTime(seconds) {
+  const minutes = Math.floor(seconds / 60); 
+  const secs = Math.floor(seconds % 60);
+  if (secs < 10) {
+    return minutes + ":0" + secs;
+  } else {
+    return minutes + ":" + secs;
+  }
+}
+
+
 playButton.addEventListener('click', function () {
   if (isPlay === false) {
-    console.log('d');
     playAudio()
     isPlay = true;
   } else {
-    console.log('h');
     pauseAudio();
     isPlay = false;
   }
@@ -40,14 +52,24 @@ function pauseAudio () {
 function changeProgressBar (e) {
   const duration = e.srcElement.duration;
   const currentTime = e.srcElement.currentTime;
-
    if (!isNaN(duration)) {
-  const progressPercent = (currentTime / duration) * 100;
-  progressBar.style.width = `${progressPercent}%`;
-   }
-  console.log("update");
+    const progressPercent = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressPercent}%`;
+  }
+
+  currentTimeElement.textContent = correctTime(currentTime);
+}
+
+function changeProgressBarAfterClick(e) {
+  const progressBarWidth = this.clientWidth;
+  const clientClick = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clientClick / progressBarWidth) * duration;
 }
 
 audio.addEventListener("timeupdate", changeProgressBar);
+progressContainer.addEventListener("click", changeProgressBarAfterClick);
 
-
+audio.addEventListener("loadedmetadata", function () {
+  trackTime.textContent = correctTime(audio.duration);
+});
